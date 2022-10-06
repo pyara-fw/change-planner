e15be781b5cb733b4a4714c258f80efa815e21a3
Eduardo Luz <eduluz1976@gmail.com>
2022-10-06 00:27:20 -0600

    Removing max length validation from Project

--- backend-src/app/Http/Controllers/Admin/ProjectController.php
+++ backend-src/app/Http/Controllers/Admin/ProjectController.php
@@ -56,7 +56,7 @@ class ProjectController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
-            'title' => 'required|max:10',
+            'title' => 'required',
             'user_id' => 'required'
         ]);
         $requestData = $request->all();
@@ -107,7 +107,7 @@ class ProjectController extends Controller
     public function update(Request $request, $id)
     {
         $this->validate($request, [
-            'title' => 'required|max:10',
+            'title' => 'required',
             'user_id' => 'required'
         ]);
         $requestData = $request->all();
