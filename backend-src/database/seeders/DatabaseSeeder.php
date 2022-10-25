018d9681d64e20fc659b4c044105cc32975ae939
Eduardo Luz <eduluz1976@gmail.com>
2022-10-25 00:09:02 -0600

    Added initial task

--- backend-src/database/seeders/DatabaseSeeder.php
+++ backend-src/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -15,26 +14,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
         DB::table('change_plans')->delete();
         DB::table('change_requests')->delete();
         DB::table('projects')->delete();
 
-        DB::table('projects')->insert([
-            'id' => 1,
-            'title' => 'Experiment 1',
-            'user_id' => 1
-        ]);
+
+        DB::table('tasks')->delete();
+        DB::table('solutions')->delete();
 
 
-        DB::table('change_requests')->insert([
-            'title' => 'Task-1: Add a list of tags on “Components” tab',
+
+        DB::table('tasks')->insert([
+            'title' => 'Add a list of tags on “Components” tab',
+            'tags' => 'feature;silver:low priority',
             'description' => '
 # Introduction
 Currently Cachet has implemented the field "tags" associated to "components" entities.
@@ -42,19 +34,16 @@ However, there is no way to see the components associated with a particular tag.
 This task intends to add this feature to Cachet.
 
 
-',
-            'project_id' => 1,
-            'reporter_user_id' => 1,
-            'assigned_user_id' => 1,
-            'status' => '1'
+'
         ]);
 
-        DB::table('change_requests')->insert([
-            'title' => 'Task-2: Fix a bug of Twig tags on incident templates',
+        DB::table('tasks')->insert([
+            'title' => 'Fix a bug of Twig tags on incident templates',
+            'tags' => 'red:bug; red:high priority;twig',
             'description' => '
-# Introduction {.hdr1}
+# Introduction
 It was found a bug related to the "incident template" feature.
-## Steps to reproduce: {.hdr2}
+## Steps to reproduce:
 1. Create an "Incident template" with a Twig tag (for example, `{{"now"|date("U")}}` );
 2. Create an "incident" selecting that template created on step above. Save it;
 3. Try to edit the incident created on previous step, clicking on "edit button".
@@ -65,14 +54,26 @@ It was found a bug related to the "incident template" feature.
 - two
 - three
 
-#  Objective {.hdr1}
+#  Objective
 The objective of this task is to fix this behaviour.
-            ',
-
-            'project_id' => 1,
-            'reporter_user_id' => 1,
-            'assigned_user_id' => 1,
-            'status' => '1'
+            '
         ]);
+
+
+        DB::table('tasks')->insert([
+    'title' => 'Add a Panic button to notify all users',
+    'tags' => 'feature;silver:low priority',
+    'description' => '
+# Introduction
+A couple of stakeholders requested a new feature, called "panic button".
+This is because in some emergencies, like a generalized attack, a lock down is triggered, and it is necessary to create an incident for all components.
+This feature will allow to send a pre-defined message to all subscribers of all managed sites, creating an incident, and setting all components as "major outage".
+
+# Requirements
+
+- Create a button on internal (authenticated) user area, with label "Panic" [Image here](https://pyara-experiment-resources.s3.amazonaws.com/tasks/task-3/task3-img1.png)
+
+'
+]);
     }
 }
